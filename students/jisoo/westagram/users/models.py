fb903100933d53508d7c987af762cbe0d34fe1e0
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-04-14 17:01:09 +0900

    users table_model

---
+++ students/jisoo/westagram/users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Users(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=300)
+    password = models.CharField(max_length=300)
+    phone_number = models.IntegerField()
