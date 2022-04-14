35245a29928ce820ee6296d62a636124d9f476ac
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-04-14 20:19:27 +0900

    users app create_2

--- students/jisoo/westagram/users/models.py
+++ students/jisoo/westagram/users/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Users(models.Model):
     name = models.CharField(max_length=45)
-    email = models.CharField(max_length=300)
+    email = models.EmailField(max_length=300)
     password = models.CharField(max_length=300)
     phone_number = models.IntegerField()
