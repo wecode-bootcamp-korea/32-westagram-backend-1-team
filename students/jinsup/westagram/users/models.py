eaae438611c3e1f1364a39977348281880d987d4
Nathan Jinsup Yoon <102127518+nathanyoon1212@users.noreply.github.com>
2022-04-14 18:55:40 +0900

    Update models.py

--- students/jinsup/westagram/users/models.py
+++ students/jinsup/westagram/users/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class User(models.Model):
-    name         = models.CharField(max_length=45)
-    email        = models.CharField(max_length=45)
-    password     = models.CharField(max_length=200)
+    username     = models.CharField(max_length=45)
+    first_name   = models.CharField(max_length=45)
+    last_name    = models.CharField(max_length=45)
+    email        = models.CharField(max_length=45, unique=True)
+    password     = models.CharField(max_length=1000)
     number       = models.CharField(max_length=45)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
+
+    
     class Meta:
         db_table = 'users'
