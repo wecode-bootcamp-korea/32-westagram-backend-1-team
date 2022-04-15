39e456960c9456d7ea90bbaf28bd48063cf43547
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-04-15 20:32:02 +0900

    signup_view_basic

--- students/jisoo/westagram/users/views.py
+++ students/jisoo/westagram/users/views.py
@@ -1,10 +1,25 @@
+
 import json
 
 from django.http import JsonResponse
 from django.views import View
 
-from signup.models import Users
+from users.models import User
 
-class Singup(View):
+class SignupView(View):
     def post(self, request):
+        try:
+            data = json.loads(request.body)
+            User.objects.create(
+                username     = ['username'],
+                first_name   = ['first_name'],
+                last_name    = ['last_name'],
+                email        = ['email'],
+                password     = ['password'],
+                phone_number = ['phone_number'],
+            )
+            return JsonResponse({'message':'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
         
+    
