d39168696f03b836a4806a4f76c29c78e3860905
전예찬 맥북에어 <yechan@jeon-yechan-ui-MacBookAir.local>
2022-04-15 19:37:56 +0900

    view update-signup

--- students/yechan/westagram/users/views.py
+++ students/yechan/westagram/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
+from nis import match
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from users.models import User
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+                # email_regex  = '^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$'
+                # passwd_regex = '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$'
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message':'email_already_exists'}, status=400)
+            # if not re.match(email_regex, data['email']):
+            #     return JsonResponse({'message':'invalid_email'}, status=400)
+            # if not re.match(passwd_regex, data['email']):
+            #     return JsonResponse({'message':'invalid_password'}, status=400)
+            User.objects.create(username    = data['username'],
+                                email       = data['email'],
+                                password    = data['password'],
+                                phonenumber = data['phonenumber']
+                                )
+            return JsonResponse({'message':'SUCCESS'},status=201)
+        except ValueError:
+            return JsonResponse({'message':'Key Error'},status=400)
