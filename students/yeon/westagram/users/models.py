5d05774fe464c28490bea85f6035ed8a3024cfae
백승연 <wdelight34@gmail.com>
2022-04-15 10:45:07 +0900

    project initial setting

--- students/yeon/westagram/users/models.py
+++ students/yeon/westagram/users/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name        = models.CharField(max_length=20)
-    email       = models.EmailField(max_length=45)
-    password    = models.CharField(max_length=20)
-    phonenumber = models.IntegerField
+    username    = models.CharField(max_length=45)
+    first_name  = models.CharField(max_length=45)
+    last_name   = models.CharField(max_length=45)
+    email       = models.EmailField(max_length=45, unique=True)
+    password    = models.CharField(max_length=500)
+    number      = models.CharField(max_length=45)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+
 
     class Meta:
         db_table = 'users'
+
