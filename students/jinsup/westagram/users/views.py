bce4eb60379cbb8bfdcd461de5b4b1700232130a
Nathan Jinsup Yoon <102127518+nathanyoon1212@users.noreply.github.com>
2022-04-16 20:19:23 +0900

    Update views.py

--- students/jinsup/westagram/users/views.py
+++ students/jinsup/westagram/users/views.py
@@ -6,26 +6,34 @@ from users.models import User
 
 class SignUpView(View):
     def post(self, request):
+        data = json.loads(request.body)
+        
+        username   = data['username']
+        first_name = data['first_name']
+        last_name  = data['last_name']
+        email      = data['email']
+        password   = data['password']
+        number     = data['number']
+
         try:
-            data           = json.loads(request.body)
-
-            if re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", data["email"]) == None:
+            if not re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", data["email"]):
                 return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
+  
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'message' : 'Email_Already_Exists'}, status=400)
 
-            if re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", data["password"]) == None:
+            if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", data["password"]):
                 return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
 
 
             User.objects.create(
-                username   = data['username'],
-                first_name = data['first_name'],
-                last_name  = data['last_name'],
-                email      = data['email'],
-                password   = data['password'],
-                number     = data['number'],
+                username   = username,
+                first_name = first_name,
+                last_name  = last_name,
+                email      = email,
+                password   = password,
+                number     = number,
             )
             return JsonResponse({'message':'created'}, status = 201)
-
-
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
