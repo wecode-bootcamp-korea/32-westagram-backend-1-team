6dd99149189b04bde8f2d8f6cc301ae20d2ca563
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-04-17 17:37:04 +0900

    signup_view_email_password

--- students/jisoo/westagram/users/views.py
+++ students/jisoo/westagram/users/views.py
@@ -1,8 +1,8 @@
 
-import json
+import json, re
 
-from django.http import JsonResponse
 from django.views import View
+from django.http  import JsonResponse
 
 from users.models import User
 
@@ -10,14 +10,30 @@ class SignupView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
+            
+            email    = data['email'],
+            password = data['password'],
+
+            email_regex    = '[a-zA-Z0-9.-_+]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+'
+            password_regex = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+
+            if not re.match(email_regex, data['email']):
+                return JsonResponse({'message' : 'INVALID_EMAIL'}, status = 400)
+            if not re.match(password_regex, data['password']):
+                return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 400)
+            
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'MESSAGE' :'ALREADY_EXISTS_EMAIL'}, status=400)
+            
             User.objects.create(
-                username     = ['username'],
-                first_name   = ['first_name'],
-                last_name    = ['last_name'],
-                email        = ['email'],
-                password     = ['password'],
-                phone_number = ['phone_number'],
+                username     = data['username'],
+                first_name   = data['first_name'],
+                last_name    = data['last_name'],
+                email        = data['email'],
+                password     = data['password'],
+                phone_number = data['phone_number'],
             )
+
             return JsonResponse({'message':'SUCCESS'}, status=201)
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
