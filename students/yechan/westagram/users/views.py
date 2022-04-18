a2b5a44bf11219b3f87075b069c274c9271398bf
전예찬 맥북에어 <yechan@jeon-yechan-ui-MacBookAir.local>
2022-04-18 15:24:54 +0900

    fix signup

--- students/yechan/westagram/users/views.py
+++ students/yechan/westagram/users/views.py
@@ -1,11 +1,9 @@
-
-from django.shortcuts import render
 import json, re
 
-from django.http import JsonResponse, HttpResponse
+from django.http  import JsonResponse, HttpResponse
 from django.views import View
+
 from users.models import User
-# from users.validation import Validate
 
 class SignUpView(View):
     def post(self, request):
@@ -18,18 +16,19 @@ class SignUpView(View):
             regex_email    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$'
             regex_password = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$"
 
-            if User.objects.filter(email=data['email']).exists():
-                return JsonResponse({'message':'email_already_exists'}, status=400)
             if not re.match(regex_email, email):
                 return JsonResponse({'message' : 'INVALID_EMAIL'}, status=400)
             if not re.match(regex_password, password):
                 return JsonResponse({'message' : 'INVALID_PASSWORD'}, status=400)
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message':'email_already_exists'}, status=400)
 
-            User.objects.create(username    = username,
-                                email       = email,
-                                password    = password,
-                                phonenumber = phonenumber
-                                )
+            User.objects.create(
+                username    = username,
+                email       = email,
+                password    = password,
+                phonenumber = phonenumber,
+            )
             return JsonResponse({'message':'SUCCESS'},status=201)
-        except ValueError:
+        except KeyError:
             return JsonResponse({'message':'Key Error'},status=400)
