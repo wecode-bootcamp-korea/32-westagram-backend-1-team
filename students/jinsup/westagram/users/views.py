79db1f1dc4f7e56735835616877fc3679e2c57d6
Nathan Jinsup Yoon <102127518+nathanyoon1212@users.noreply.github.com>
2022-04-18 11:22:41 +0900

    Update views.py

--- students/jinsup/westagram/users/views.py
+++ students/jinsup/westagram/users/views.py
@@ -1,30 +1,31 @@
-import json, re
-from django.http import JsonResponse
+import json, re # 파이썬 built-in package
+
+from django.http import JsonResponse # third-party package
 from django.views import View
-from users.models import User
+
+from users.models import User   #직접 만든 package
 
 
 class SignUpView(View):
     def post(self, request):
-        data = json.loads(request.body)
-        
-        username   = data['username']
-        first_name = data['first_name']
-        last_name  = data['last_name']
-        email      = data['email']
-        password   = data['password']
-        number     = data['number']
-
         try:
+            data = json.loads(request.body)
+            
+            username   = data['username']
+            first_name = data['first_name']
+            last_name  = data['last_name']
+            email      = data['email']
+            password   = data['password']
+            number     = data['number']
+
             if not re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", data["email"]):
-                return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
-  
+                return JsonResponse({'message': 'INVALID_EMAIL'}, status=400) 
+
             if User.objects.filter(email = email).exists():
                 return JsonResponse({'message' : 'Email_Already_Exists'}, status=400)
 
             if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", data["password"]):
                 return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
-
 
             User.objects.create(
                 username   = username,
