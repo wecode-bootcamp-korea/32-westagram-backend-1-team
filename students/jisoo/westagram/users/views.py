b0cbd24f920b3f2fff4989c81f079069c5b600ef
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-04-18 16:31:39 +0900

    login_view

--- students/jisoo/westagram/users/views.py
+++ students/jisoo/westagram/users/views.py
@@ -39,4 +39,20 @@ class SignupView(View):
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
         
-    
+class LoginView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            email    = data['email'],
+            password = data['password'],
+
+            if User.objects.filter(email = email).exists():
+                    return JsonResponse({'message':'INVALID_USER'}, status=401)
+
+            if User.objects.filter(password = password).exists():
+                return JsonResponse({'message':'INVALID_USER'}, status=401)
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
