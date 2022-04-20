4e8ec47e892fcb58c90d53815b66ee24fc40e9b6
전예찬 맥북에어 <yechan@jeon-yechan-ui-MacBookAir.local>
2022-04-20 21:22:24 +0900

    Add: JWT

--- students/yechan/westagram/users/views.py
+++ students/yechan/westagram/users/views.py
@@ -1,7 +1,9 @@
-import json, re, bcrypt
+import json, re, bcrypt, jwt
 
 from django.http  import JsonResponse
 from django.views import View
+from django.conf  import settings
+
 from users.models import User
 
 class SignUpView(View):
@@ -36,13 +38,21 @@ class SingInView(View):
     def post(self, request):
         try:
             data     = json.loads(request.body)
-            email    = data['email']
-            password = data['password']
+            user     = User.objects.get(email=data['email'])
 
-            if User.objects.filter(email=email,password=password).exists():
-                return JsonResponse({'message':'SUCCESS'},status=201)
-            else:
-                 return JsonResponse({'message':'INVALID_USER'},status=401)
+            if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message':'INVALID_PASSWORD'},status=401)
+
+            access_token = jwt.encode({'id': user.id}, settings.SECRET_KEY, algorithm = settings.ALGORITHM)
+
+            return JsonResponse({
+                    'message'      : 'SUCCESS',
+                    'access_token' : access_token,
+                },status=200)
 
         except KeyError:
-            return JsonResponse({'message':'KEY_ERROR'},status=400)
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'message':'VALUE_ERROR'}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({"message" : "INVALID_EMAIL"}, status=401)
