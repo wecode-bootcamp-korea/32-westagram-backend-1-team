a9ea111e37c2d311bb1f0f0c5cff63ccb0f1271c
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-04-21 16:43:34 +0900

    jwt

--- students/jisoo/westagram/users/views.py
+++ students/jisoo/westagram/users/views.py
@@ -4,6 +4,7 @@ import json, re, bcrypt, jwt
 from django.views     import View
 from django.http      import JsonResponse
 from django.db.models import Q
+from my_settings      import SECRET_KEY, ALGORITHM
 
 from users.models     import User  
 
@@ -45,15 +46,13 @@ class LoginView(View):
         try:
             data = json.loads(request.body)
 
-            email            = data['email']
-            password         = data['password']
+            user            = User.object.get(email=data['email'])
+            hashed_password = user.password.encode('uft-8')
+            access_token    = jwt.encode({'id': user.id}, SECRET_KEY, algorithm=ALGORITHM)
 
-            if not User.objects.filter(Q(email = email) & Q(password = password)).exists():
-                return JsonResponse({'message':'INVALID_USER'}, status=401)
-
-            if not User.objects.filter(password):
-                return JsonResponse({'message': "INVALID_PASSWORD"}, status=401)
+            if not bcrypt.checkpw(data['password'].encode('utf-8'), hashed_password):
+                return JsonResponse({'message': "INVALID_USER"}, status=401)
             
-            return JsonResponse({'message':'SUCCESS'}, status=200)
+            return JsonResponse({'message':'SUCCESS'}, access_token, status=200)
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
