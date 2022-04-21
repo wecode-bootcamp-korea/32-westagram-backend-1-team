68ec7beaf3bb1c2db8cec0e97d6bfbbd604f6392
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-04-21 20:18:00 +0900

    jwt_2

--- students/jisoo/westagram/users/views.py
+++ students/jisoo/westagram/users/views.py
@@ -1,20 +1,18 @@
 
 import json, re, bcrypt, jwt
 
-from django.views     import View
-from django.http      import JsonResponse
-from django.db.models import Q
-from my_settings      import SECRET_KEY, ALGORITHM
+from django.views import View
+from django.http  import JsonResponse
+from django.conf  import settings
 
-from users.models     import User  
+from users.models import User  
 
 class SignupView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
             
-            email            = data['email']
-            hashed_password  = bcrypt.hashpw((data['password']).encode('utf-8'), bcrypt.gensalt())
+            email = data['email']
 
             EMAIL_REGEX    = '[a-zA-Z0-9.-_+]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+'
             PASSWORD_REGEX = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
@@ -28,12 +26,14 @@ class SignupView(View):
             if User.objects.filter(email = email).exists():
                 return JsonResponse({'MESSAGE' :'ALREADY_EXISTS_EMAIL'}, status=400)
             
+            hashed_password  = bcrypt.hashpw((data['password']).encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
             User.objects.create(
                 username     = data['username'],
                 first_name   = data['first_name'],
                 last_name    = data['last_name'],
                 email        = data['email'],
-                password     = hashed_password.decode('utf-8)'),
+                password     = hashed_password,
                 phone_number = data['phone_number']
             )
 
@@ -46,13 +46,17 @@ class LoginView(View):
         try:
             data = json.loads(request.body)
 
-            user            = User.object.get(email=data['email'])
-            hashed_password = user.password.encode('uft-8')
-            access_token    = jwt.encode({'id': user.id}, SECRET_KEY, algorithm=ALGORITHM)
+            user            = User.objects.get(email=data['email'])
+            hashed_password = user.password.encode('utf-8')
+            access_token    = jwt.encode({'id': user.id}, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
             if not bcrypt.checkpw(data['password'].encode('utf-8'), hashed_password):
                 return JsonResponse({'message': "INVALID_USER"}, status=401)
             
-            return JsonResponse({'message':'SUCCESS'}, access_token, status=200)
+            return JsonResponse({'message':'SUCCESS', 'token': access_token}, status=200)
+
+        except User.DoesNotExist:
+            return JsonResponse({"MESSAGE": "INVALID_USER"}, status=404)
+
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
