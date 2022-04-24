5d94c510d324ac3e6bb8a6d488cd83b210333d05
wdelight34 <wdelight34@gmail.com>
2022-04-24 23:07:25 +0900

    Add: login_

--- students/sengyeon/westagram/users/views.py
+++ students/sengyeon/westagram/users/views.py
@@ -1,15 +1,16 @@
 import json
 
-from django.http       import JsonResponse
-from django.views      import View
+import bcrypt
 
-from users.models      import User
+from django.http            import JsonResponse
+from django.views           import View
+
+from users.models           import User
 
 from django.core.exceptions import ValidationError
 from users.validators       import (
-                                validate_email,
+                                UserEmailValidation,
                                 validate_password,
-                                exist_email
                             )
 
 
@@ -18,18 +19,24 @@ class SignUpView(View):
         try:
             data = json.loads(request.body)
 
+            name         = data['name']
             email        = data['email']
             password     = data['password']
             phone_number = data['phone_number']
 
-            validate_email(email)
+            email_check = UserEmailValidation()
+
+            email_check.regex(email)
+            email_check.duplicated(email)
+
             validate_password(password)
-            exist_email(phone_number)
+
+            hashed_password = bcrypt.hashpw(data['password'].encode('UTF-8'), bcrypt.gensalt())
 
             User.objects.create(
-                name         = data['name'],
+                name         = name,
                 email        = email,
-                password     = password,
+                password     = hashed_password,
                 phone_number = phone_number
             )
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
@@ -39,3 +46,20 @@ class SignUpView(View):
 
         except ValidationError as error:
             return JsonResponse({'message' : error.message}, status=error.code)
+
+
+class LogInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            email    = data['email']
+            password = data['password']
+
+            if not User.objects.filter(email=email, password=password).exists():
+                return JsonResponse({'message' : 'INVALID_USER'}, code=401)
+
+            return JsonResponse({'message' : 'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
